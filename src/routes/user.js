0fa5525b337e56b4d63f03f74474985e36d93591
wasifa aktar <wasifa.git.acc@gmail.com>
2024-10-03 17:29:19 +0530

    added feed API and pagination

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const userRoute = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 userRoute.get("/user/request",userAuth,async (req,res)=>{
     try{
         const loggedUser = req.user;
@@ -39,6 +40,42 @@ userRoute.get("/user/connections",userAuth,async (req,res)=>{
         res.status(400).send("Error:" + err.message);
     }
     
-})
+});
+userRoute.get("/user/feed",userAuth,async(req,res)=>{
+    try{
+        const loggedUser = req.user;
+        //pagination
+        const page = parseInt(req.query.page)||1;
+        const limit = parseInt(req.query.limit)||10;
+        limit = (limit > 50 ? 50 :limit);
+        const skip = (page - 1) * limit;
+
+        const invalidConnection = await ConnectionRequest.find({
+            $or: [
+                { fromUserId:loggedUser._id},{toUserId:loggedUser._id}
+            ]
+        });
+        const invalidSet = new Set();
+        invalidConnection.forEach((conn)=>{
+            invalidSet.add( conn.fromUserId.toString());
+            invalidSet.add(conn.toUserId.toString()) ;           
+        });
+        const validUser = await User.find({
+            $and:[ 
+                {_id: {$ne:loggedUser._id}
+                },
+                { _id: {$nin: Array.from(invalidSet) }
+                }
+            ]
+        }).select(["firstName","lastName"]).skip(skip).limit(limit);
+
+        res.status(200).json({
+            message:"feed data",
+            data: validUser    });
+
+    }catch(err){
+        res.status(400).send("Error:" + err.message);
+    }
+});
 
 module.exports=userRoute;
